58e73b02b95747816dc73bc06ffcf71c008e04a7
Sean Velasco <seanvelasco@ieee.org>
2024-07-14 21:05:35 +0800

    Temporary type for session storage localstorage

--- src/storage/session.ts
+++ src/storage/session.ts
@@ -2,7 +2,9 @@ import { makePersisted } from '@solid-primitives/storage'
 import { createStore } from 'solid-js/store'
 import type { Session } from '../types'
 
-const [session, setSession] = makePersisted<Session>(createStore(), { name: 'session' })
+// To-do: correctly type below. <Session | Record<string, string>> is obviously incorrect, but is currently working
+
+const [session, setSession] = makePersisted(createStore<Session | Record<string, string>>(), { name: 'session' })
 
 export {
     session,
